d09e412f2325b3bd39c4e9ad2fc59b4862483512
Andy Gill <andygill@ku.edu>
2009-12-02 14:23:28 -0600

    deleting old doc

--- doc/scripts/Index.pl
+++
@@ -1,9 +0,0 @@
-# perl script
-print ('<html><body>');
-foreach (@ARGV) {
-    /^(\d+_)(.*).txt$/;
-    print ('<a href="' . "$1$2" . '.html' . '">' . $2 . '</a>');
-    print (' (<a href="' . "$1$2" . '.txt' . '">txt</a>)');
-    print "<BR>\n";
-}
-print ('</body></html>');
