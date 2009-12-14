527cf84987f9ad94c82eb50fb85b4c07ecf40206
Andy Gill <andygill@ku.edu>
2009-12-14 11:46:48 -0600

    First cut at a video demo, of just importing and drawing buffers.

--- tests/video1/Main.hs
+++ tests/video1/Main.hs
@@ -2,18 +2,19 @@ module Main where
 
 import Graphics.ChalkBoard
 import Graphics.ChalkBoard.Video
+import Control.Concurrent
 
-
-main = startChalkBoard [] videoMain
+main = startChalkBoard [BoardSize (480*2) (360*2)] videoMain
 
 
 videoMain cb = do
---    videoPipe <- openVideoInPipe "ffmpeg -i bigfoot.mpeg -f image2pipe -vcodec ppm  -"
-    videoPipe <- openVideoInPipe "cat bigfoot.ppm"
+    videoPipe <- openVideoInPipe "ffmpeg -i bigfoot.mpeg -f image2pipe -vcodec ppm  -"
+--    videoPipe <- openVideoInPipe "cat bigfoot.ppm"
     let loop 500 = exitChalkBoard cb
 	loop n = do
     	(worked, buffer) <- nextPPMFrame videoPipe
-    	drawChalkBuffer cb buffer
+    	drawChalkBuffer cb (boardToBuffer (0,0) (480*2,360*2) $ move (0,360*2) $ scaleXY (2,-2) $ bufferOnBoard buffer (boardOf white))
+--	threadDelay (1000 * 1000)
 	loop (n+1)
 	
     loop 0
