2b7498bd071de29c63daacc05fc3b0a1d117d170
Andy Gill <andygill@ku.edu>
2010-01-04 23:13:42 -0600

    Adding font support!

---
+++ Graphics/ChalkBoard/Font.hs
@@ -0,0 +1,90 @@
+module Graphics.ChalkBoard.Font
+	( Graphics.ChalkBoard.Font.initFont
+	, Font
+	, letter
+	, label
+	) where
+
+
+import Data.Boolean
+
+import Data.Ix
+import Data.Array.Unboxed
+import Graphics.Rendering.TrueType.STB hiding (Font)
+import qualified Graphics.Rendering.TrueType.STB as STB
+import Graphics.ChalkBoard
+import qualified Data.ByteString as BS
+
+data Font = Font
+	STB.Font		-- the font
+	()			-- the cache for each use char
+
+initFont :: String -> Int -> IO Font
+initFont fontFile ix = do
+    tt <- loadTTF fontFile
+    en <- enumerateFonts tt
+    font <- STB.initFont tt (en !! ix)
+    return $ Font font ()
+
+label :: Font -> Float -> String -> IO (Board UI)
+label font sz str = do
+	
+	let brd0 :: Board (Maybe UI)
+	    brd0 = flip withMask false <$> boardOf 0.9
+
+	brds <- sequence [ do
+		(b,off) <- letter font sz ch
+		return (just <$> b,off)
+		| ch <- str
+		]
+
+	let lens :: [Float]
+	    lens = 0 : Prelude.zipWith (+) (map snd brds) lens
+	let brd1 :: Board (Maybe UI)
+	    brd1 = foldr (\ (buff,off) brd -> buff `bufferOnBoard` (move (off,0) brd)) brd0 
+			(Prelude.zip (map fst brds) (map id (map snd brds)))
+
+	return $ withDefault 0.0 <$> brd1
+
+letter :: Font -> Float -> Char -> IO 
+	( Buffer UI		-- 
+	, Float			-- how far to push rest of word to right
+	)
+letter (Font font ()) sz ch = do
+    Just glyph_K <- findGlyph font ch
+    bb_K <- getGlyphBoundingBox font glyph_K
+    (bm_K,bo_K) <- newGlyphBitmap font glyph_K (sz,sz)
+    m_K <- getGlyphHorizontalMetrics font glyph_K
+    bma_K <- bitmapArray bm_K 
+
+    -- The backing board must be a power of two (PoT).
+    let pot' n x = if n > x then n else pot' (n * 2) x
+    let pot = pot' 1
+
+    let ((x0,y0),(x1,y1)) = bounds bma_K
+
+    let x1' = pot x1 - 1
+    let y1' = pot y1 - 1
+
+    let bs' = BS.pack [ if x > x1 || y > y1 then 0 else bma_K ! (x1 - (x - x0),y)
+		      | x <- [x0..x1']
+		      , y <- [y0..y1']
+		      ]
+
+    let x1' = x1 
+    let y1' = y1
+    let (x1'',y1'') = (pot x1' - 1,pot y1' - 1)
+
+    let (bo_x,bo_y) = bo_K
+
+    let (BBox (a,b) (c,d)) = bb_K
+    xx <- getGlyphBitmapBox font glyph_K (sz,sz)
+    let (BBox (a,b) (c,d)) = xx
+
+    return 
+	( moveBuffer (0 + ceiling (sz * fromIntegral (leftSideBearing m_K)),-d) $ newBufferUI bs' (y1''+1,x1''+1)
+	, sz * fromIntegral (advanceWidth m_K) -- + the remainer from the ceiling operation
+	)
+
+just :: O UI -> O (Maybe UI)
+just o = withMask o (o /=* 0)
