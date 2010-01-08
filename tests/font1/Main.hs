715105d660c396633f596bec69fe01b2567727ab
Kevin Matlage <kmatlage@scotland.ittc.ku.edu>
2010-01-07 18:42:07 -0600

    First draft of font test pretty finished. Lots that can be improved. Some things still don't work great.

--- tests/font1/Main.hs
+++ tests/font1/Main.hs
@@ -76,7 +76,7 @@ instance Applicative O where {}
 main = do
      	font <- Font.initFont "../../Arial.ttf" 0
      	let (w,h) = (800,600)
-	startChalkBoard [BoardSize w h] $ \ cb -> main2 cb font 0.04 (w,h)
+	startChalkBoard [BoardSize w h] $ \ cb -> main2 cb font 0.03 (w,h)
 
 
 main2 cb font sz (w,h) = do
@@ -86,51 +86,59 @@ main2 cb font sz (w,h) = do
 	--print titleSP
 	
 		
-	(point1, sp1) <- Font.label font sz ([chr 0x25cf] ++ " KU supplied 4 ''binaries'', one for each problem.")
-        (point2, sp2) <- Font.label font sz ([chr 0x25cf] ++ " We also provided specification of small virtual machine, to run these binaries.")
-        (point3, sp3) <- Font.label font sz ([chr 0x25cf] ++ " Contestants write the VM, then interact with virtual actuators to fire rockets " ++
-                                                                "and virtual sensors to detect location in orbit.")
-        (point4, sp4) <- Font.label font sz ([chr 0x25cf] ++ " Contestants upload an audit trail of what actuator fires when.")
-        (point5, sp5) <- Font.label font sz ([chr 0x25cf] ++ " We replay these on our local VM, validate the score, and update a leader board.")
-        (point6, sp6) <- Font.label font sz ([chr 0x25cf] ++ " At the end of the contest, teams upload their final source files.")
-        (point7, sp7) <- Font.label font sz ([chr 0x25cf] ++ " We further evaluate/validate the top 10 scoring entries to determine the winners.")
+	(point1, sp1) <- Font.label font (0.5*sz) ([chr 0x25cf] ++ " KU supplied 4 ''binaries'', one for each problem.")
+        (point2, sp2) <- Font.label font (0.5*sz) ([chr 0x25cf] ++ " We also provided specification of small virtual machine, to run these binaries.")
+        (point3, sp3) <- Font.label font (0.5*sz) ([chr 0x25cf] ++ " Contestants write the VM, then interact with the virtual actuators and sensors.")
+                                                             --" Contestants write the VM, then interact with virtual actuators to fire rockets " ++
+                                                             --  "and virtual sensors to detect location in orbit.")
+        (point4, sp4) <- Font.label font (0.5*sz) ([chr 0x25cf] ++ " Contestants upload an audit trail of what actuator fires when.")
+        (point5, sp5) <- Font.label font (0.5*sz) ([chr 0x25cf] ++ " We replay these on our local VM, validate the score, and update a leader board.")
+        (point6, sp6) <- Font.label font (0.5*sz) ([chr 0x25cf] ++ " At the end of the contest, teams upload their final source files.")
+        (point7, sp7) <- Font.label font (0.5*sz) ([chr 0x25cf] ++ " We further evaluate and validate the top 10 scoring entries to determine the winners.")
 	--}
         
-        let over2 a b = over b a
+        --let over2 a b = over b a
         
-	drawChalkBoard cb (mix black white <$> (
-	                                        (move (0,0.4) $ center (w,h) titleSP $ adjustTitle (w,h) titleSP title)
-	                                        `over`
-	                                        (move (-0.45,0.0) $ adjustPoint (w,h) sp1 point1)
-	                                       )
-	                  )
+        let slide = mix black white <$> (
+	                                  (makeTitle (w,h) titleSP title)
+	                                  `over`
+	                                  (makeBullets (w,h) sp sp1 [point1, point2, point3, point4, point5, point6, point7])
+	                                )
+              
+        let scaling = (fromIntegral h) / (fromIntegral w)
+            scaledSlide = if (w > h)
+                              then scaleXY (scaling,1) $ slide
+                              else scaleXY (1,1.0/scaling) $ slide
+        
+	drawChalkBoard cb scaledSlide
 
-        return ()
+
         
 
-center :: (Int,Int) -> Float -> Board UI -> Board UI
-center (wboard,hboard) size lbl = move (x,0) $ lbl
-                                  where
-                                        x = if (wboard > hboard)
-                                                then -0.5 -- + (((fromIntegral wboard) - (fromIntegral hboard)::R) / (2.0 * (fromIntegral wboard)))
-                                                else -0.5
 
 
-adjustTitle :: (Int,Int) -> Float -> Board UI -> Board UI
-adjustTitle (wboard,hboard) size lbl = if (wboard > hboard)
-                                           then scaleXY (xscale,1) $ scale (0.9 * (1/size)) $ lbl
-                                           else scaleXY (1,yscale) $ scale (0.9 * (1/size)) $ lbl
-                                       where 
-                                           xscale = (fromIntegral hboard) / (fromIntegral wboard)
-                                           yscale = (fromIntegral wboard) / (fromIntegral hboard)
 
-adjustPoint :: (Int,Int) -> Float -> Board UI -> Board UI
-adjustPoint (wboard,hboard) size lbl = if (wboard > hboard)
-                                           then scaleXY (xscale,1) $ scale (0.7 * (1/size)) $ lbl
-                                           else scaleXY (1,yscale) $ scale (0.7 * (1/size)) $ lbl
-                                       where 
-                                           xscale = (fromIntegral hboard) / (fromIntegral wboard)
-                                           yscale = (fromIntegral wboard) / (fromIntegral hboard)
+makeTitle :: (Int,Int) -> Float -> Board UI -> Board UI
+makeTitle (wboard,hboard) size lbl =  scale (0.9) $ move (-0.5015,0.45) $ scale (1/size) $ lbl
+
+                                       
+makeBullets :: (Int,Int) -> Float -> Float -> [Board UI] -> Board UI
+makeBullets (w,h) sp size lbls = makeBullets_ (w,h) sp size lbls 0
+
+makeBullets_ :: (Int,Int) -> Float -> Float -> [Board UI] -> Int -> Board UI
+makeBullets_ (_,_) _ _ [] _ = (boardOf 0)
+makeBullets_ (w,h) sp size (lbl:lbls) lines = (makeBullet (w,h) sp size lines lbl) `over` (makeBullets_ (w,h) sp size lbls (lines+1))
+
+makeBullet :: (Int,Int) -> Float -> Float -> Int -> Board UI -> Board UI
+makeBullet (w,h) sp size lines lbl = if (w > h)
+                                         then move (-(1-fontSize)/2-(1-scaling)/2,0) $ scale (fontSize) $ move (-0.5015,spacing) $ scale (1/size) $ lbl
+                                         else move (-(1-fontSize)/2,0) $ scale (fontSize) $ move (-0.5015,spacing) $ scale (1/size) $ lbl
+                                     
+                                     where  fontSize = 0.6
+                                            spacing = 0.25 - (fromIntegral lines)*perLine
+                                            perLine = (fromIntegral h)/sp*6/(fromIntegral h)--(sp)/(fromIntegral h)
+                                            scaling = (fromIntegral h) / (fromIntegral w)
+                                           
 
 
 
