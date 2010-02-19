ac7742809728da353d3606b3e53f6a3b8cfc64cb
Kevin Matlage <kmatlage@scotland.ittc.ku.edu>
2010-02-19 16:31:41 -0600

    Working triangle animation with a lot of different timing and ordering possibilities

--- tests/triAnim/Main.hs
+++ tests/triAnim/Main.hs
@@ -2,46 +2,54 @@ module Main where
 
 import Graphics.ChalkBoard as CB
 import Control.Applicative (pure)
-import System.IO.Unsafe
+--import System.IO.Unsafe
 
 
 main = startChalkBoard [BoardSize (400) (400)] animMain
 
 
 animMain cb = do
-        let background = pure $ boardOf (alpha white) :: Active (Board (RGBA->RGBA))
-            triangle345 = pure $ choose (alpha black) transparent <$> triangle (0,0) (0,-0.3) (0.4,-0.3) :: Active (Board (RGBA->RGBA))
-            square4  = pure $ move (0.2,-0.5) $ scale 0.4 $ choose (withAlpha 0.6 blue) transparent <$> square :: Active (Board (RGBA->RGBA))
+        --Set up the animations
+        let triangle345 = pure $ choose (alpha black) transparent <$> triangle (0,0) (0,-0.3) (0.4,-0.3) :: Active (Board (RGBA->RGBA))
             
-        let moveSq3s = [ taking 2 (moveSq3 curSq i) | (curSq,i) <- Prelude.zip square3s [0..] ]
+            moveSq3s = [ taking 0.5 (moveSq3 curSq i) | (curSq,i) <- Prelude.zip square3s [0..] ]
             square3s = [ scale 0.3 $ scale (1/3) $ choose (withAlpha 0.6 red) transparent <$> square | (x,y) <- combinations [0..2] [0..2] ]
             
-            --moveSq3s = [ taking 2 $ color red (moveSq3 curSq i) | (curSq,i) <- Prelude.zip square3s [0..] ]
-            --square3s = [ scale 0.3 $ scale (1/3) $ choose 0.6 0 <$> square | (x,y) <- combinations [0..2] [0..2] ]
+            moveSq4s = [ taking 0.3 (moveSq4 curSq i) | (curSq,i) <- Prelude.zip square4s [0..] ]
+            square4s = [ scale 0.4 $ scale (1/4) $ choose (withAlpha 0.6 blue) transparent <$> square | (x,y) <- combinations [0..3] [0..3] ]
             
-            --square3  = overList moveSq3s
-            --square3  = mix white red <$> {-myMix <$> (CB.zip (CB.zip (boardOf (withAlpha 0.6 red)) (boardOf transparent))-} (withDefault 0.0 <$> (overList init3pos))
-            --moveSq3s = [ unsafePerformIO (moveSquare (moveSq3 curSq i)) | (curSq,i) <- Prelude.zip square3s [0..] ]
-            --init3pos = map (\x -> move (-0.15,-0.15) $ scale 0.3 x) square3s
-            --square3s = [ move (x/3,y/3) $ move (-1/3,-1/3) $ scale (1/3) $ {-withMask 0.6 <$>-} square | (x,y) <- combinations [0..2] [0..2] ]
+            moveRec4s = [ taking 1 (moveRec4 curSq i) | (curSq,i) <- Prelude.zip rectangle4s [0..] ]
+            rectangle4s = [ scale 0.4 $ scaleXY (1/4,1) $ choose (withAlpha 0.6 blue) transparent <$> square | (x,y) <- combinations [0..1] [0..1] ]
             
-            
-        let animation = (for 18 $ flicker
-		                ( [ taking 0 $ triangle345
-		                  , taking 0 $ square4
-		                  ] ++
-		                  moveSq3s
-		                )
-	                )
+            -- could use something instead of combinations or actually do something with them?
+            -- could start square moves from their start locations and then move from 0,0 to final location?
+            -- could potentially use UI instead, not that it would be helpful
+        
 	
-        let loop 0 = return ()
-            loop n = do
-                animatedScene <- moveScene animation -- $ square4 `over` square3 `over` triangle345 `over` background
-                let scaledScene = scale 0.8 $ move (-0.2,0.15) $ animatedScene
-                drawChalkBoard cb $ unAlphaBoard (boardOf white) scaledScene
-                loop (n-1)
-        loop 100
-        --}
+	--Determine the ordering/timing to create different looking animations
+	let normalAnim = for 6 $ (flicker moveSq4s) `over` (flicker moveSq3s) `over` triangle345
+	    allAtOnce = for 6 $ (taking 1 $ overList moveSq4s) `over` (taking 1 $ overList moveSq3s) `over` triangle345
+	    redThenBlue = for 8 $ flicker $ [taking 0 triangle345] ++ (map (taking 0.3) moveSq3s) ++ moveSq4s
+	    blueThenRed = for 8 $ flicker $ [taking 0 triangle345] ++ moveSq4s ++ (map (taking 0.3) moveSq3s)
+	    rectangles = for 6 $ (flicker moveRec4s) `over` (flicker moveSq3s) `over` triangle345
+	    rectsAtOnce = for 6 $ (taking 1 $ overList moveRec4s) `over` (taking 1 $ overList moveSq3s) `over` triangle345
+	
+	--Pick the animation you would like to see and turn it into a play object
+	playObj <- byFrame 29.97 normalAnim
+        --playObj <- realTime active
+        
+        
+        --Run the animation
+        let loop = do
+                mbScene <- play playObj
+                case mbScene of
+                        Just scene -> do
+                                let scaledScene = scale 0.8 $ move (-0.2,0.15) $ scene
+                                drawChalkBoard cb $ unAlphaBoard (boardOf white) scaledScene
+                                loop
+                        Nothing  -> return ()
+        
+        loop
         
         exitChalkBoard cb
 
@@ -56,45 +64,61 @@ combinationsR (a:as) (b:bs) = (a,b):(combinationsR (a:as) bs)
 combinationsR _ _ = []
 
 
-moveScene active = do
-                frame <- byFrame 29.97 active
-                --frame <- realTime active
-                mbBrd <- play frame
-                finalBrd <- case mbBrd of
-                                Just brd -> return brd
-                                Nothing  -> return $ boardOf transparent
-                return finalBrd
-
 
 moveSq3 :: Board a -> Int -> Active (Board a)
 moveSq3 sq i = moveBrd sq (pi/2-atan(4/3)) (x*0.1-0.25,y*0.1-0.25) (x*0.1+0.15,y*0.1+0.15)
         where x = fromIntegral (i `mod` 3)
               y = fromIntegral (i `div` 3)
 
+moveSq4 :: Board a -> Int -> Active (Board a)
+moveSq4 sq i = moveBrd sq (pi/2-atan(4/3)) (x*0.1+0.05,-y*0.1-0.35) ((fx i)*0.1+0.05,(fy i)*0.1+0.05)
+        where x = fromIntegral (i `mod` 4)
+              y = fromIntegral (i `div` 4)
+              fx index | index < 5  = fromIntegral index
+                       | index > 10 = fromIntegral (index-11)
+                       | odd index  = 0
+                       | even index = 4
+              fy index | index < 5   = 0
+                       | index < 7   = 1
+                       | index < 9   = 2
+                       | index < 11  = 3
+                       | index >= 11 = 4
+
+moveRec4 :: Board a -> Int -> Active (Board a)
+moveRec4 rec i = case num of
+        0 -> moveBrd rec angle        (num*0.1+0.05,0.1-0.6) (0.05,0.2)
+        1 -> moveBrd rec (angle+pi/2) (num*0.1+0.05,0.1-0.6) (-0.05,0.2+0.1)
+        2 -> moveBrd rec angle        (num*0.1+0.05,0.1-0.6) (0.05+0.4,0.2+0.1)
+        3 -> moveBrd rec (angle+pi/2) (num*0.1+0.05,0.1-0.6) (-0.05-0.4,0.2)
+        where angle = (pi/2-atan(4/3))
+              num = fromIntegral i
+
+
+
 moveBrd :: Board a -> Radian -> Point -> Point -> Active (Board a)
-moveBrd brd rot (sx,sy) (ex,ey) = mkActive 1 brd activeBoth
+moveBrd brd rot (sx,sy) (ex,ey) = mkActive brd activeBoth
         where activeMove ui = move (ui*(ex-sx)+sx,ui*(ey-sy)+sy)
               activeRot ui  = rotate (ui * rot)
-              activeBoth ui board = (activeMove ui) ((activeRot ui) board)
+              activeBoth ui board = (activeRot ui) $ (activeMove ui) board
 
 
-mkActive :: R -> (Board a) -> (UI -> Board a -> Board a) -> Active (Board a)
-mkActive t brd fn = fmap (\ ui -> (fn ui) $ brd)
-	            $ scale t
-	            $ age
+
+mkActive :: (Board a) -> (UI -> Board a -> Board a) -> Active (Board a)
+mkActive brd fn = fmap (\ ui -> (fn ui) $ brd) age
 
 
-color :: O RGB -> Active (Board UI) -> Active (Board (RGBA -> RGBA))
-color rgb = fmap ((\ ui -> withAlpha ui rgb) .$)
 
 
 
 {-
-mkActive, color, overList, moveScene
+mkActive, color, overList, unfilledPolygon?
 moveToOrigin? or a localRotate? or a moveToPosition?
+compiler optimizations would be great since we are often manipulating the same board over and over
 
 
 
+color :: O RGB -> Active (Board UI) -> Active (Board (RGBA -> RGBA))
+color rgb = fmap ((\ ui -> withAlpha ui rgb) .$)
 
 let fn :: O ((RGB,RGB),Bool) -> O RGB
 fn o = choose (fstO (fstO o)) (sndO (fstO o)) (sndO o)
