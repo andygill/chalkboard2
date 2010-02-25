27a293d16f6114f256fdc0cc421f94919454d6c5
Kevin Matlage <kmatlage@scotland.ittc.ku.edu>
2010-02-25 16:30:09 -0600

    Working pythagorean animation. Slow because of all the different animation elements.

--- tests/pythagoreanAnim/Main.hs
+++ tests/pythagoreanAnim/Main.hs
@@ -7,61 +7,64 @@ import Control.Applicative (pure)
 main = do
         font <- initFont "../../Arial.ttf" 0
         let (w,h) = (400,400)
-        startChalkBoard [BoardSize w h] (\cb ->  animMain cb font 0.03 (w,h))
+        startChalkBoard [BoardSize w h] (\cb ->  animMain cb font 0.01 (w,h))
 
 
 animMain cb font sz (w,h) = do
+        --Set up the font labals
         (aLabel,aSP) <- label font sz ("a")
         (bLabel,bSP) <- label font sz ("b")
         (cLabel,cSP) <- label font sz ("c")
         (areaLabel,areaSP) <- label font sz ("area = c²")
         (formulaLabel,formulaSP) <- label font sz ("c² = a² + b²")
         
-        
+        --Set up the different parts of the animation
         let background = pure $ boardOf $ withAlpha 0.2 yellow :: Active (Board (RGBA->RGBA))
         
             triangle345 = triangle (-0.2,0.15) (-0.2,-0.15) (0.2,-0.15)
             triLines = pointsToLine [(-0.2,0.15), (-0.2,-0.15), (0.2,-0.15), (-0.2,0.15)] 0.004
             mainTriangle = (choose (alpha black) transparent <$> triLines) `over` (choose (alpha yellow) transparent <$> triangle345)
-            
             largeTriangle = mergeActive (activeMove (0.15,0.2)) $ mkActive (activeScale (2/3)) $ scale 1.5 $ mainTriangle
-            a = makelbl sz $ move (-0.25,0) aLabel
-            b = makelbl sz $ move (0,-0.2) bLabel
-            c = makelbl sz $ move (0.03,0.03) cLabel
+            
+            a = move (-0.25,-0.03) $ makelbl aSP aLabel
+            b = move (-0.04,-0.195) $ makelbl bSP bLabel
+            c = move (0.005,0.005) $ makelbl cSP cLabel
+            firstABC = mkActive tempAppear $ (move (-0.1,0) a) `over` (move (-0.03,-0.08) b) `over` (move (0.005,0.005) c)
 
             otherTriangles = [ rotate (-i*pi/2) $ move (0.15,0.2) $ mainTriangle | i <- [1..3] ]
             otherTrianglesIn = overList [actMove i $ mkActive appear $ {-mkActive (activeMove (1,0)) $ move (-1,0) $-} tri | (tri,i) <- Prelude.zip otherTriangles [1..] ]
             
-            colorSquare = fadeIn 1 yellow $ scale 0.095 $ square
+            colorSquare = mkActive appear $ scale 0.095 $ choose (alpha yellow) transparent <$> square
+            area = mkActive appear $ move (-0.4,0.35) $ makelbl aSP areaLabel
+            secondABC = mkActive tempAppear $ move (0.15, 0.2) $ (move (0.06,0) a) `over` (move (0,0.06) b) `over` c `over` (move (0.05,-0.38) c)
             
             fadedTriangles = [ rotate (-i*pi/2) $ move (0.15,0.2) $ choose (withAlpha 0.6 white) transparent <$> triangle345 | i <- [0,1] ]
             slideLeft = mergeActive appear $ (mkActive (activeMove (-0.3,-0.4)) $ move (0.15,0.2) $ mainTriangle) `over` (pure (head fadedTriangles))
             slideRight = mergeActive appear $ (mkActive (activeMove (0.4,-0.3)) $ (head otherTriangles)) `over` (pure (last fadedTriangles))
             
+            thirdABC = mkActive tempAppear $ (move (-0.15, -0.2) $ a `over` b) `over` (move (0.45,-0.36) a) `over` (move (0.4,0) b)
+            
             square4 = move (0.15, -0.15) $ scale 0.4 $ square
             square3 = move (-0.2, -0.2) $ scale 0.3 $ square
             squares = (square4 `over` square3)
-            
-            fadeSquares = fadeIn 0.5 white squares
-            
             newLines = pointsToLine [(-0.05,-0.35), (0.35,-0.35), (0.35,0.05), (-0.05,0.05), (-0.05,-0.35), (-0.35,-0.35), (-0.35,-0.05), (-0.05,-0.05)] 0.004
             newSquares = (fadeIn 1 black newLines) `over` (fadeIn 0.9 yellow squares)
+            
+            finalABC = mkActive appear $ (move (-0.15, -0.2) a) `over` (move (0.03,-0.36) a) `over` (move (0.18,-0.2) b) `over` (move (0.4,0) b)
+            formula = mkActive appear $ move (0.15,0.35) $ makelbl aSP formulaLabel
 
-        let anim = for 10 $ flicker [ taking 0.5 $ background
-                                    , taking 0.5 $ a `over` b `over` c
-                                    , taking 1 $ largeTriangle
-                                    , wait 0.5
-                                    , taking 1 $ otherTrianglesIn  -- Need to fade this in or something
-                                    , taking 0.5 $ colorSquare
-                                    , wait 0.5
-                                    , taking 1 $ slideLeft
-                                    , wait 0.5
-                                    , taking 1 $ slideRight
-                                    --, taking 0.5 $ fadeSquares
-                                    , wait 0.5
-                                    , taking 1 $ newSquares
-                                    , wait 0.5
-                                    ]
+        --Set up the animation ordering/timing
+        let anim = flicker [ taking 0.5 $ background
+                           , taking 0.5 $ firstABC
+                           , taking 1 $ largeTriangle
+                           , wait 0.5
+                           , taking 1 $ otherTrianglesIn
+                           , taking 0.5 $ colorSquare `over` secondABC `over` area
+                           , taking 1 $ slideLeft
+                           , taking 1 $ slideRight
+                           , taking 1 $ newSquares `over` thirdABC
+                           , taking 1 $ finalABC `over` formula
+                           ]
         
         
         --Pick the animation you would like to see and turn it into a play object
@@ -95,12 +98,17 @@ appear ui = if ui == 0
                 then scale 0
                 else scale 1
 
+tempAppear ui = if (ui == 0 || ui == 1)
+                        then scale 0
+                        else scale 1
+
 
 mergeActive :: (UI -> Board a -> Board a) -> Active (Board a) -> Active (Board a)
 mergeActive fn (Active start stop f) = Active start stop (\ui -> (fn (fromRational ui) . f) ui)
 
 mkActive :: (UI -> Board a -> Board b) -> (Board a) -> Active (Board b)
 mkActive fn brd = fmap (\ ui -> (fn ui) $ brd) age
+
 
 
 fadeIn :: UI -> O RGB -> Board Bool -> Active (Board (RGBA -> RGBA))
@@ -112,14 +120,25 @@ wait n = taking n (pure (boardOf transparent))
 
 
 
-makelbl :: Float -> Board UI -> Active (Board (RGBA -> RGBA))
-makelbl size lbl =  pure $ color black $ scale (0.7) $ scale (1/size) $ lbl
+makelbl :: Float -> Board UI -> Board (RGBA -> RGBA)
+makelbl size lbl =  color black $ scale (0.7) $ scale (1/(size*25)) $ lbl
 
 color :: O RGB -> Board UI -> Board (RGBA -> RGBA)
-color rgb brd = (\ ui -> withAlpha ui rgb) .$ brd
+color rgb brd = (\ ui -> withAlpha ui rgb) <$> brd
+
+
+
 
 
 {-
+makelbl2 :: Float -> Board UI -> Active (Board (RGBA -> RGBA))
+makelbl2 size lbl =  color2 black $ pure $ scale (0.7) $ scale (1/(size*15)) $ lbl
+
+color2 :: O RGB -> Active (Board UI) -> Active (Board (RGBA -> RGBA))
+color2 rgb = (\b -> fmap ((\ ui2 -> withAlpha (ui2) rgb) .$) b)
+
+
+
 newActiveMove :: Point -> Board a -> Active (Board a)
 activeMove (x,y) brd = mkActive brd activeMv
         where activeMv ui = move (ui*x,ui*y)
