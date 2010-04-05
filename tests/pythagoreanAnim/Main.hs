7bf7d735549e3974e17f83e013a77bec1d2c988a
Kevin Matlage <kmatlage@scotland.ittc.ku.edu>
2010-04-05 11:13:13 -0500

    Some more changes to the Main file of the pythagorean anim so that it might be easier to follow

--- tests/pythagoreanAnim/Main.hs
+++ tests/pythagoreanAnim/Main.hs
@@ -26,45 +26,47 @@ animMain cb font sz (w,h) = do
             mainTriangle = (choose (alpha black) transparent <$> triLines) `over` (choose (alpha yellow) transparent <$> triangle345)
             movingTriangle = activeMove (0.15,0.2) $ activeScale (2/3) $ pure $ scale 1.5 $ mainTriangle
             
-            a = move (-0.25,-0.03) $ makelbl aSP aLabel
-            b = move (-0.04,-0.195) $ makelbl bSP bLabel
-            c = move (0.005,0.005) $ makelbl cSP cLabel
-            firstABC = pure $ (move (-0.1,0) a) `over` (move (-0.03,-0.08) b) `over` (move (0.005,0.005) c)
-            
             otherTriangles = [ rotate (-i*pi/2) $ move (0.15,0.2) $ mainTriangle | i <- [1..3] ]
-            otherTrianglesActive = overList [actMove i $ activeAppear $ pure $ {-activeMove (1,0) $ move (-1,0) $-} tri | (tri,i) <- Prelude.zip otherTriangles [1..] ]
+            addInOtherTriangles = overList [actMove i $ activeAppear $ pure $ tri | (tri,i) <- Prelude.zip otherTriangles [1..] ]
             
-            colorSquare = activeAppear $ pure $ scale 0.095 $ choose (alpha yellow) transparent <$> square
-            area = activeAppear $ pure $ move (-0.4,0.35) $ makelbl aSP areaLabel
-            secondABC = activeTempAppear $ pure $ move (0.15, 0.2) $ (move (0.06,0) a) `over` (move (0,0.06) b) `over` c `over` (move (0.05,-0.38) c)
+            fillInSquare = activeAppear $ pure $ scale 0.095 $ choose (alpha yellow) transparent <$> square
             
             fadedTriangles = [ rotate (-i*pi/2) $ move (0.15,0.2) $ choose (withAlpha 0.6 white) transparent <$> triangle345 | i <- [0,1] ]
             slideLeft = activeAppear $ (activeMove (-0.3,-0.4) $ pure $ move (0.15,0.2) $ mainTriangle) `over` (pure (head fadedTriangles))
             slideRight = activeAppear $ (activeMove (0.4,-0.3) $ pure $ (head otherTriangles)) `over` (pure (last fadedTriangles))
             
-            thirdABC = activeTempAppear $ pure $ (move (-0.15, -0.2) $ a `over` b) `over` (move (0.45,-0.36) a) `over` (move (0.4,0) b)
-            
-            square4 = move (0.15, -0.15) $ scale 0.4 $ square
-            square3 = move (-0.2, -0.2) $ scale 0.3 $ square
-            squares = (square4 `over` square3)
+            newSquares = (move (0.15, -0.15) $ scale 0.4 $ square) `over` (move (-0.2, -0.2) $ scale 0.3 $ square)
             newLines = pointsToLine [(-0.05,-0.35), (0.35,-0.35), (0.35,0.05), (-0.05,0.05), (-0.05,-0.35), (-0.35,-0.35), (-0.35,-0.05), (-0.05,-0.05)] 0.004
-            newSquares = (fadeIn 1 black newLines) `over` (fadeIn 0.8 yellow squares)
+            fadeInSquares = (fadeIn 1 black newLines) `over` (fadeIn 0.8 yellow newSquares)
+        
+        
+        let a = move (-0.25,-0.03) $ makelbl aSP aLabel
+            b = move (-0.04,-0.195) $ makelbl bSP bLabel
+            c = move (0.005,0.005) $ makelbl cSP cLabel
+            firstABC = activeTempAppear $ pure $ (move (-0.1,0) a) `over` (move (-0.03,-0.08) b) `over` (move (0.005,0.005) c)
+            
+            areaEq = activeAppear $ pure $ move (-0.4,0.35) $ makelbl aSP areaLabel
+            secondABC = activeTempAppear $ pure $ move (0.15, 0.2) $ (move (0.06,0) a) `over` (move (0,0.06) b) `over` c `over` (move (0.05,-0.38) c)
+            
+            thirdABC = activeTempAppear $ pure $ (move (-0.15, -0.2) $ a `over` b) `over` (move (0.45,-0.36) a) `over` (move (0.4,0) b)
             
             finalABC = activeAppear $ pure $ (move (-0.15, -0.2) a) `over` (move (0.03,-0.36) a) `over` (move (0.18,-0.2) b) `over` (move (0.4,0) b)
             formula = activeAppear $ pure $ move (0.15,0.35) $ makelbl aSP formulaLabel
+            
+            
         
         --Set up the animation ordering/timing
         let anim = flicker [ taking 0.5 $ background
-                           , taking 1 $ activeTempAppear $ firstABC
+                           , taking 1 $ firstABC
                            , taking 1 $ movingTriangle
                            , wait 0.5
-                           , taking 2 $ otherTrianglesActive
-                           , taking 1.5 $ colorSquare `over` secondABC `over` area
+                           , taking 2 $ addInOtherTriangles
+                           , taking 1.5 $ fillInSquare `over` secondABC `over` areaEq
                            , taking 1 $ slideLeft
                            , wait 0.5
                            , taking 1 $ slideRight
                            , taking 1 $ thirdABC
-                           , taking 1 $ newSquares `over` thirdABC
+                           , taking 1 $ fadeInSquares `over` thirdABC
                            , taking 3 $ finalABC `over` formula
                            ]
         
@@ -73,7 +75,7 @@ animMain cb font sz (w,h) = do
 	playObj <- byFrame 29.97 anim
         
         --Start the video write stream
-        --sid <- startDefaultWriteStream cb "pythagorean.avi"
+        sid <- startDefaultWriteStream cb "pythagorean.avi"
         
         --Run the animation
         let loop = do
@@ -81,13 +83,13 @@ animMain cb font sz (w,h) = do
                 case mbScene of
                         Just scene -> do
                                 drawChalkBoard cb $ unAlphaBoard (boardOf white) scene
-                                --frameChalkBoard cb sid
+                                frameChalkBoard cb sid
                                 loop
                         Nothing  -> return ()
         
         loop
         
-        --endWriteStream cb sid
+        endWriteStream cb sid
         exitChalkBoard cb
 
 
